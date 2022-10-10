e5cdd4078784dcf0ebe27880b35fca48907b6f1c
carol.ouellet@gmail.com <carol.ouellet@gmail.com>
2022-10-10 18:46:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+Process p = new Process();
+p.StartInfo.FileName = "cmd.exe";
+p.StartInfo.Arguments = "/c C:\\Windows\\System32\\ncpa.cpl";
+p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+p.StartInfo.RedirectStandardOutput = true;
+p.StartInfo.RedirectStandardError = true;
+p.StartInfo.CreateNoWindow = true;
+
+p.Start();
